ce077bb9830cea630251cb45fda40225acbe32cb
Kitill Tilicyn <ogromniymladenec@gmail.com>
2026-07-08 12:22:04 +0300

    Создание поля и конструктора для класса Seller

--- Seller.cs
+++ Seller.cs
@@ -4,6 +4,18 @@ namespace CityFair
 {
     public class Seller
     {
+        private string _name;
 
+        public Seller(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                _name = "Иван Иванов";
+            }
+            else
+            {
+                _name = name;
+            }
+        }
     }
 }
