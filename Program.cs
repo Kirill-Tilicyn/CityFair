904856af4857e8a7d70e7c51b078a8a698ea3395
Kitill Tilicyn <ogromniymladenec@gmail.com>
2026-07-08 12:40:00 +0300

    Добавление в Program enum MenuAction

--- Program.cs
+++ Program.cs
@@ -4,21 +4,33 @@ namespace CityFair
 {
     internal class Program
     {
-         public static void Main(string[] args)
+        private enum MenuAction
+        {
+            AddFair = 1,
+            AddSeller = 2,
+            RegisterSeller = 3,
+            RemoveSeller = 4,
+            ShowFairs = 5,
+            ShowSellers = 6,
+            ShowAll = 7,
+            Exit = 8
+        }
+
+        public static void Main(string[] args)
         {
             bool isProgramWorking = true;
 
             while (isProgramWorking)
             {
                 Console.WriteLine("Выберите действие:");
-                Console.WriteLine("1 - Добавить новую ярмарку");
-                Console.WriteLine("2 - Добавить нового продавца");
-                Console.WriteLine("3 - Зарегистрировать продавца на ярмарку");
-                Console.WriteLine("4 - Снять продавца с ярмарки");
-                Console.WriteLine("5 - Просмотреть список доступных ярмарок");
-                Console.WriteLine("6 - Просмотреть список продавцов");
-                Console.WriteLine("7 - Просмотреть все ярмарки и зарегистрированных продавцов");
-                Console.WriteLine("8 - Завершить работу приложения");
+                Console.WriteLine($" - Добавить новую ярмарку");
+                Console.WriteLine($"2 - Добавить нового продавца");
+                Console.WriteLine($"3 - Зарегистрировать продавца на ярмарку");
+                Console.WriteLine($"4 - Снять продавца с ярмарки");
+                Console.WriteLine($"5 - Просмотреть список доступных ярмарок");
+                Console.WriteLine($"6 - Просмотреть список продавцов");
+                Console.WriteLine($"7 - Просмотреть все ярмарки и зарегистрированных продавцов");
+                Console.WriteLine($"8 - Завершить работу приложения");
                 Console.Write("Выберите вариант действия: ");
                 string userNumberText = Console.ReadLine().Trim();
                 bool isUserNumberValid = int.TryParse(userNumberText, out int userNumber);
