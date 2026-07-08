6d152b70659a9dba2599c53950845eb4d9e0a8c2
Kitill Tilicyn <ogromniymladenec@gmail.com>
2026-07-08 12:21:26 +0300

    Создание поля и конструктора для класса Fair

--- Fair.cs
+++ Fair.cs
@@ -4,6 +4,18 @@ namespace CityFair
 {
     public class Fair
     {
+        private string _name;
 
+        public Fair(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                _name = "Зарубежная ярмарка";
+            }
+            else
+            {
+                _name = name;
+            }
+        }
     }
 }
