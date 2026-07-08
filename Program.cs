86b6751f39ae6b50b9cbcbdfd7f17a7cafb6936e
Kitill Tilicyn <ogromniymladenec@gmail.com>
2026-07-08 13:02:33 +0300

    Удаление switch-case и переход на if-else. Создание функций RequestNameFair и RequestNameSeller в Program

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO.Pipes;
 
 namespace CityFair
 {
@@ -39,49 +40,93 @@ namespace CityFair
 
                 if (isUserNumberValid)
                 {
-                    switch (userNumber)
+                    if (userNumber == (int)MenuAction.AddFair)
                     {
-                        case (int)MenuAction.AddFair:
-                            //FairManager.AddFair();
-                            break;
+                        string nameFair = "";
 
-                        case (int)MenuAction.AddSeller:
-                            //SellerManager.AddSeller();
-                            break;
+                        bool isNameFairValid = RequestNameSeller(ref nameFair);
 
-                        case (int)MenuAction.RegisterSeller:
-                            //FairManager.RegisterSellerToFair();
-                            break;
+                        if (isNameFairValid)
+                        {
+                            Console.WriteLine("Ярмарка создана!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Вы ввели некоректные данные! Ярмарка не создана!");
+                        }
+                    }
+                    else if (userNumber == (int)MenuAction.AddSeller)
+                    {
+                        string nameSeller = "";
 
-                        case (int)MenuAction.RemoveSeller:
-                            //FairManager.RemoveSellerFromFair();
-                            break;
+                        bool isNameSellerValid = RequestNameSeller(ref nameSeller);
 
-                        case (int)MenuAction.ShowFairs:
-                            //FairManager.ShowFairs();
-                            break;
+                        if (isNameSellerValid)
+                        {
+                            Console.WriteLine("Продавец создан!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Вы ввели некоректные данные! Продавец не создан!");
+                        }
+                    }
+                    else if (userNumber == (int)MenuAction.RegisterSeller)
+                    {
 
-                        case (int)MenuAction.ShowSellers:
-                            //SellerManager.ShowSellers();
-                            break;
+                    }
+                    else if (userNumber == (int)MenuAction.RemoveSeller)
+                    {
 
-                        case (int)MenuAction.ShowAll:
-                            //FairManager.ShowAllFairsAndSellers();
-                            break;
+                    }
+                    else if (userNumber == (int)MenuAction.ShowFairs)
+                    {
 
-                        case (int)MenuAction.Exit:
-                            isProgramWorking = false;
-                            break;
-
-                        default:
-                            Console.WriteLine("Некорректный выбор. Пожалуйста, выберите действие из списка.");
-                            break;
+                    }
+                    else if (userNumber == (int)MenuAction.ShowSellers)                    
+                    {
+                        
+                    }
+                    else if (userNumber == (int)MenuAction.ShowAll)
+                    {
+                    }
+                    else if (userNumber == (int)MenuAction.Exit)
+                    {
+                        isProgramWorking = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Некорректный выбор. Пожалуйста, выберите действие из списка.");
                     }
                 }
             }
 
             Console.WriteLine("Завершение работы приложения...");
             Console.ReadKey();
+        }
+
+        public static bool RequestNameFair(ref string nameFair)
+        {
+            Console.Write("Введите название ярмарки: ");
+            nameFair = Console.ReadLine()?.Trim();
+
+            if (string.IsNullOrEmpty(nameFair))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool RequestNameSeller(ref string nameSeller)
+        {
+            Console.Write("Введите имя продавца: ");
+            nameSeller = Console.ReadLine()?.Trim();
+
+            if (string.IsNullOrEmpty(nameSeller))
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
