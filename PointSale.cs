25bb6a350c67b6a4c32d533ae7b246b8fb581da1
Kitill Tilicyn <ogromniymladenec@gmail.com>
2026-07-11 20:23:04 +0300

    Создана заготовка класса CityFair.

---
+++ PointSale.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CityFair
+{
+    internal class PointSale
+    {
+        private string _name;
+
+        public PointSale(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                _name = "Точка продажи";
+            }
+            else
+            {
+                _name = name;
+            }
+        }
+    }
+}
