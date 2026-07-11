a1ef98a238ab83b440fd7f990c0025296810d8bb
Kitill Tilicyn <ogromniymladenec@gmail.com>
2026-07-11 20:52:14 +0300

    Класс Fair cоздано новое поле _points и осуществлено его создание через констркутор. Созданы методы GetFair и AddPointSale.

--- Fair.cs
+++ Fair.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CityFair
 {
     public class Fair
     {
         private string _name;
+        private List<PointSale> _points;
 
         public Fair(string name)
         {
@@ -16,6 +18,24 @@ namespace CityFair
             {
                 _name = name;
             }
+
+            _points = new List<PointSale>();
+        }
+
+        public string GetFair()
+        {
+            return _name;
+        }
+
+        public bool AddPointSale(string namePointSale)
+        {
+            if (string.IsNullOrEmpty(namePointSale))
+            {
+                return false;
+            }
+
+            _points.Add(new PointSale(namePointSale));
+            return true;
         }
     }
 }
